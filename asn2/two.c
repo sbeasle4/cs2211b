e15ff6e551ba587486c4c5166558c6211b6e5851
sbeasle4 <sbeasle4@uwo.ca>
2013-02-17 22:04:11 -0500

    Update asn2/two.c
    comments

--- asn2/two.c
+++ asn2/two.c
@@ -1,6 +1,15 @@
+/* Asignment 2
+ * part TWO
+ * Purpose: C program that calculates an exponential number using recursion
+ * Author: Steve Beasley */
+
 #include <stdio.h>
 
 float exp_calculator(float x, int a);
+
+// The main function will ask for the base and the exponent and then 
+// call our exp_calculator function to find the sum. Then will neatly
+// print the sum to the screen.
 
 int main()
 {
@@ -8,18 +17,19 @@ int main()
     int a;
 
     printf("What is the base? : ");
-    scanf("%f", &x);
+    scanf("%f", &x);                        //get the base
     printf("What is the exponent? : ");
-    scanf("%d", &a);
+    scanf("%d", &a);                        //get the exponent
 
     float sum;
-    sum = exp_calculator(x,a);
+    sum = exp_calculator(x,a);              //calculate the sum
 
-    printf("Result: %.2f\n", sum);
+    printf("Result: %.2f\n", sum);          //print results
 
     return 0;
 }
 
+// the exp_calculator takes a float base and an int exponent and then returns the value of the number.
 float exp_calculator(float x, int a)
 {
     if (a==0)
